8371fd2158c599c2132c02a8fb6599f3c7ee836d
Usuario Alvaro <lamadrid774@gmail.com>
2019-03-03 00:53:03 +0100

    eliminar sobras

--- psd/Pract/Practica2/datos.h
+++
@@ -1,4 +0,0 @@
-#define N 6
-#define M 1000000
-int flag1 = 0;
-int flag2 = 0;
--- psd/Pract/Practica3/datos.h
+++
@@ -1,4 +0,0 @@
-#define M 10//filas
-#define N 4 // columnas
-#define K 10 // rango de valores
-#define V 5 //iteraciones por hilo de relleno
