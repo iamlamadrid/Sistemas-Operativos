54b4390f1b2c2a05178e774ca041844db3c195d1
Usuario Alvaro <lamadrid774@gmail.com>
2019-05-08 18:07:19 +0200

    Ajedrez inicio

---
+++ practica3/ajedrez.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <unistd.h>
+
---
+++ practica3/constantes.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <time.h>
+#include <malloc.h>
+#include <pthread.h>
+
+#define TAB 64
+#define NUM_MOV 0
+#define NUM_CAP 0
+#define NUM_PTS 0
